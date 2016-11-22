7d2638196a58f114f75f87d3503fd6431119245c
Ben S <allobon@gmail.com>
2016-11-22 16:28:00 +0000

    react build

---
+++ 2-webpack-react/webpack.config.js
@@ -0,0 +1,22 @@
+function getEntrySources(sources) {
+    if (process.env.NODE_ENV !== "production") {
+        sources.push("webpack-dev-server/client?http://localhost:8080");
+        sources.push("webpack/hot/only-dev-server");
+    }
+
+    return sources;
+}
+
+module.exports = {
+    entry: getEntrySources(["./src/js/entry.js"]),
+    output: {
+        publicPath: "http://localhost:8080/",
+        filename: "public/build/bundle.js"
+    },
+    module: {
+        loaders: [
+            { test: /\.js$/, loaders: ['react-hot', 'jsx', 'babel'], exclude: /node_modules/ },
+            { test: /\.scss$/, loaders: ['style', 'css', 'sass'] }
+        ]
+    }
+};
