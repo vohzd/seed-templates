648e142463241a59c207041473ba52a6b5abcfbf
Ben S <allobon@gmail.com>
2016-11-24 17:46:50 +0000

    important config files

---
+++ 4-webpack-react-server/src/js/entry.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Main from "./Main/MainComponent.js";
+
+console.log("i think this is running....");
+
+ReactDOM.render(<Main />, document.getElementById("injection-point"));
