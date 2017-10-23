7cf6c52645d15a6da38c96eeb7cdbae2400d3701
Ben <vohzd@protonmail.com>
2017-10-23 16:32:22 +0100

    latest boiler

---
+++ 14-oct-2017-babel-updates/src/components/Settings/Settings.vue
@@ -0,0 +1,25 @@
+<template lang="html">
+  <section>
+    <h2>Settings</h2>
+  </section>
+</template>
+
+<script>
+/*
+ * VUEX HELPERS
+ */
+import { mapGetters }               from "vuex";
+
+/*
+ * OTHER LIBS
+ */
+import moment                       from "moment";
+
+export default {
+
+}
+</script>
+
+<style lang="scss">
+
+</style>
